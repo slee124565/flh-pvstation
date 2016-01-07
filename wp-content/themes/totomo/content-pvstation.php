31529309de5f9fe2cc3c83b89ba5b500aa215e99
Lee Shiueh <lee.shiueh@gmail.com>
2016-01-07 16:23:58 +0800

    part energy information from database display on homepage

--- wp-content/themes/totomo/content-pvstation.php
+++ wp-content/themes/totomo/content-pvstation.php
@@ -4,6 +4,82 @@
  *
  * @package totomo
  */
+    global $wpdb;
+    
+    $PVI_NAME = "H5";
+    $MINUTE_TIME_RAGE_QUERY = 30;
+    $INPUT_REGISTER_ADDRESS_VOLTAGE = 1057;
+    $INPUT_REGISTER_ADDRESS_CURRENT = 1058;
+    $INPUT_REGISTER_ADDRESS_WATTAGE = 1059;
+    $INPUT_REGISTER_ADDRESS_ENERGY_TODAY = 1072;
+    $INPUT_REGISTER_ADDRESS_DC_LIFE_WH = 1076;
+    $t_ac_output_voltage_sql = "select value from pvi_regdata where 
+                address = $INPUT_REGISTER_ADDRESS_VOLTAGE
+                and pvi_name = '$PVI_NAME'
+                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
+                order by date desc
+                limit 1";
+    $t_ac_output_voltage = $wpdb->get_var($t_ac_output_voltage_sql);
+    if ($t_ac_output_voltage) {
+        $t_ac_output_voltage = $t_ac_output_voltage * 0.1;
+    } else {
+        $t_ac_output_voltage = 'N/A';
+    }
+    $t_ac_output_current_sql = "select value from pvi_regdata where 
+                address = $INPUT_REGISTER_ADDRESS_CURRENT
+                and pvi_name = '$PVI_NAME'
+                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
+                order by date desc
+                limit 1";
+    $t_ac_output_current = $wpdb->get_var($t_ac_output_current_sql);
+    if ($t_ac_output_current) {
+        $t_ac_output_current = $t_ac_output_current * 0.01;
+    } else {
+        $t_ac_output_current = 'N/A';
+    }
+    $t_ac_output_wattage_sql = "select value from pvi_regdata where 
+                address = $INPUT_REGISTER_ADDRESS_WATTAGE
+                and pvi_name = '$PVI_NAME'
+                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
+                order by date desc
+                limit 1";
+    $t_ac_output_wattage = $wpdb->get_var($t_ac_output_wattage_sql);
+    if (! $t_ac_output_wattage) {
+        $t_ac_output_wattage = 'N/A';
+    }
+    $t_dc_life_output_wattage_sql = "select value from pvi_regdata where 
+                address = $INPUT_REGISTER_ADDRESS_DC_LIFE_WH
+                and pvi_name = '$PVI_NAME'
+                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
+                order by date desc
+                limit 1";
+    $t_dc_life_output_wattage = $wpdb->get_var($t_dc_life_output_wattage_sql);
+    if ($t_dc_life_output_wattage) {
+        $t_dc_life_output_wattage = $t_dc_life_output_wattage * 0.01;
+        $t_dc_lift_carbon_low = $t_dc_life_output_wattage * 0.637;
+        $t_dc_life_profit = $t_dc_life_output_wattage * 6.8633;
+    } else {
+        $t_dc_life_output_wattage = 'N/A';
+        $t_dc_lift_carbon_low = 'N/A';
+        $t_dc_life_profit = 'N/A';
+    }
+    $t_energy_today_sql = "select value from pvi_regdata where 
+                address = $INPUT_REGISTER_ADDRESS_ENERGY_TODAY
+                and pvi_name = '$PVI_NAME'
+                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
+                order by date desc
+                limit 1";
+    $t_energy_today = $wpdb->get_var($t_energy_today_sql);
+    if ($t_energy_today) {
+        $t_energy_today = $t_energy_today * 0.01;
+        $t_energy_today_carbon_low = $t_energy_today * 0.637;
+        $t_energy_today_profit = $t_energy_today * 6.8633;
+    } else {
+        $t_energy_today = 'N/A';
+        $t_energy_today_carbon_low = 'N/A';
+        $t_energy_today_profit = 'N/A';
+    }
+    //var_dump($t_dc_life_output_wattage_sql);exit;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -20,9 +96,9 @@
                     <tr><th></th><th>今日</th><th>本月</th><th>總計</th></tr>
                 </thead>
                 <tbody>
-                    <tr><th>總累計發電量(kWh)</th><td>181.00</td><td>2999.00</td><td>193398.00</td></tr>
-                    <tr><th>減碳量(kg)</th><td>1448.00</td><td>23992.00</td><td>123456.00</td></tr>
-                    <tr><th>收益(NTD)</th><td>1448.00</td><td>23992.00</td><td>123456.00</td></tr>
+                    <tr><th>總累計發電量(kWh)</th><td><?php echo sprintf("%.2f",$t_energy_today); ?></td><td>N/A</td><td><?php echo sprintf("%.2f",$t_dc_life_output_wattage); ?></td></tr>
+                    <tr><th>減碳量(kg)</th><td><?php echo sprintf("%.2f", $t_energy_today_carbon_low); ?></td><td>N/A</td><td><?php echo sprintf("%.2f",$t_dc_lift_carbon_low); ?></td></tr>
+                    <tr><th>收益(NTD)</th><td><?php echo sprintf("%.2f",$t_energy_today_profit); ?></td><td>N/A</td><td><?php echo sprintf("%.2f",$t_dc_life_profit); ?></td></tr>
                 </tbody>
             </table>
         </div>
@@ -32,17 +108,17 @@
                 <div class="col-md-6">
                     <table class="table">
                         <tr ><th class="text-center" colspan="3">輸出</th></tr>
-                        <tr><th>電壓</th><td>235.28</td><th>V</th></tr>
-                        <tr><th>電流</th><td>18.71</td><th>A</th></tr>
-                        <tr><th>輸出功率</th><td>8.06</td><th>KW</th></tr>
+                        <tr><th>電壓</th><td><?php echo $t_ac_output_voltage; ?></td><th>V</th></tr>
+                        <tr><th>電流</th><td><?php echo $t_ac_output_current; ?></td><th>A</th></tr>
+                        <tr><th>輸出功率</th><td><?php echo $t_ac_output_wattage; ?></td><th>W</th></tr>
                     </table>
                 </div>
                 <div class="col-md-6">
                     <table class="table">
                         <tr><th class="text-center" colspan="3">環境條件</th></tr>
-                        <tr><th>日照強度</th><td>235.28</td><th>W/m2</th></tr>
-                        <tr><th>環境溫度</th><td>18.71</td><th>C</th></tr>
-                        <tr><th>模板溫度</th><td>8.06</td><th>C</th></tr>
+                        <tr><th>日照強度</th><td>N/A</td><th>W/m2</th></tr>
+                        <tr><th>環境溫度</th><td>N/A</td><th>C</th></tr>
+                        <tr><th>模板溫度</th><td>N/A</td><th>C</th></tr>
                     </table>
                 </div>
             </div>
