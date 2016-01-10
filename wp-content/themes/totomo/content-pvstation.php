be7f1c324f34e64e79027cc3100bb9d949282dd4
Lee Shiueh <lee.shiueh@gmail.com>
2016-01-10 22:29:27 +0800

    integrate pvappengin pvs_meta json data

--- wp-content/themes/totomo/content-pvstation.php
+++ wp-content/themes/totomo/content-pvstation.php
@@ -6,80 +6,14 @@
  */
     global $wpdb;
     
-    $PVI_NAME = "H5";
-    $MINUTE_TIME_RAGE_QUERY = 30;
-    $INPUT_REGISTER_ADDRESS_VOLTAGE = 1057;
-    $INPUT_REGISTER_ADDRESS_CURRENT = 1058;
-    $INPUT_REGISTER_ADDRESS_WATTAGE = 1059;
-    $INPUT_REGISTER_ADDRESS_ENERGY_TODAY = 1072;
-    $INPUT_REGISTER_ADDRESS_DC_LIFE_WH = 1076;
-    $t_ac_output_voltage_sql = "select value from pvi_regdata where 
-                address = $INPUT_REGISTER_ADDRESS_VOLTAGE
-                and pvi_name = '$PVI_NAME'
-                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
-                order by date desc
-                limit 1";
-    $t_ac_output_voltage = $wpdb->get_var($t_ac_output_voltage_sql);
-    if (is_null($t_ac_output_voltage)) {
-        $t_ac_output_voltage = 'N/A';
-    } else {
-        $t_ac_output_voltage = $t_ac_output_voltage * 0.1;
-    }
-    $t_ac_output_current_sql = "select value from pvi_regdata where 
-                address = $INPUT_REGISTER_ADDRESS_CURRENT
-                and pvi_name = '$PVI_NAME'
-                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
-                order by date desc
-                limit 1";
-    $t_ac_output_current = $wpdb->get_var($t_ac_output_current_sql);
-    if (is_null($t_ac_output_current)) {
-        $t_ac_output_current = 'N/A';
-    } else {
-        $t_ac_output_current = $t_ac_output_current * 0.01;
-    }
-    $t_ac_output_wattage_sql = "select value from pvi_regdata where 
-                address = $INPUT_REGISTER_ADDRESS_WATTAGE
-                and pvi_name = '$PVI_NAME'
-                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
-                order by date desc
-                limit 1";
-    $t_ac_output_wattage = $wpdb->get_var($t_ac_output_wattage_sql);
-    if (is_null($t_ac_output_wattage)) {
-        $t_ac_output_wattage = 'N/A';
-    }
-    $t_dc_life_output_wattage_sql = "select value from pvi_regdata where 
-                address = $INPUT_REGISTER_ADDRESS_DC_LIFE_WH
-                and pvi_name = '$PVI_NAME'
-                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
-                order by date desc
-                limit 1";
-    $t_dc_life_output_wattage = $wpdb->get_var($t_dc_life_output_wattage_sql);
-    if (is_null($t_dc_life_output_wattage)) {
-        $t_dc_life_output_wattage = 'N/A';
-        $t_dc_lift_carbon_low = 'N/A';
-        $t_dc_life_profit = 'N/A';
-    } else {
-        $t_dc_life_output_wattage = $t_dc_life_output_wattage * 0.01;
-        $t_dc_lift_carbon_low = $t_dc_life_output_wattage * 0.637;
-        $t_dc_life_profit = $t_dc_life_output_wattage * 6.8633;
-    }
-    $t_energy_today_sql = "select value from pvi_regdata where 
-                address = $INPUT_REGISTER_ADDRESS_ENERGY_TODAY
-                and pvi_name = '$PVI_NAME'
-                and date between date_add(now(), interval -$MINUTE_TIME_RAGE_QUERY MINUTE) and now()
-                order by date desc
-                limit 1";
-    $t_energy_today = $wpdb->get_var($t_energy_today_sql);
-    if (is_null($t_energy_today)) {
-        $t_energy_today = 'N/A';
-        $t_energy_today_carbon_low = 'N/A';
-        $t_energy_today_profit = 'N/A';
-    } else {
-        $t_energy_today = $t_energy_today * 0.01;
-        $t_energy_today_carbon_low = $t_energy_today * 0.637;
-        $t_energy_today_profit = $t_energy_today * 6.8633;
-    }
-    //var_dump($t_dc_life_output_wattage_sql);exit;
+    $t_siteurl_sql = 'select option_value from wp_options where option_name = "siteurl"';
+    $t_siteurl = $wpdb->get_var($t_siteurl_sql);
+
+    $pvi_name = 'H5';
+    $pvs_meta_json = file_get_contents($t_siteurl . "/appeng/pvs_meta/");
+    $pvs = json_decode($pvs_meta_json);
+    //var_dump($pvs->dc_output->H5);exit;
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -96,9 +30,18 @@
                     <tr><th></th><th>今日</th><th>本月</th><th>總計</th></tr>
                 </thead>
                 <tbody>
-                    <tr><th>總累計發電量(kWh)</th><td><?php echo sprintf("%.2f",$t_energy_today); ?></td><td>N/A</td><td><?php echo sprintf("%.2f",$t_dc_life_output_wattage); ?></td></tr>
-                    <tr><th>減碳量(kg)</th><td><?php echo sprintf("%.2f", $t_energy_today_carbon_low); ?></td><td>N/A</td><td><?php echo sprintf("%.2f",$t_dc_lift_carbon_low); ?></td></tr>
-                    <tr><th>收益(NTD)</th><td><?php echo sprintf("%.2f",$t_energy_today_profit); ?></td><td>N/A</td><td><?php echo sprintf("%.2f",$t_dc_life_profit); ?></td></tr>
+                    <tr><th>總累計發電量(kWh)</th>
+                        <td><?php echo $pvs->pvs_static->today->total_eng_kwh; ?></td>
+                        <td><?php echo $pvs->pvs_static->this_month->total_eng_kwh; ?></td>
+                        <td><?php echo $pvs->pvs_static->until_now->total_eng_kwh; ?></td></tr>
+                    <tr><th>減碳量(kg)</th>
+                        <td><?php echo $pvs->pvs_static->today->total_carbon_save; ?></td>
+                        <td><?php echo $pvs->pvs_static->this_month->total_carbon_save; ?></td>
+                        <td><?php echo $pvs->pvs_static->until_now->total_carbon_save; ?></td></tr>
+                    <tr><th>收益(NTD)</th>
+                        <td><?php echo $pvs->pvs_static->today->total_income; ?></td>
+                        <td><?php echo $pvs->pvs_static->this_month->total_income; ?></td>
+                        <td><?php echo $pvs->pvs_static->until_now->total_income; ?></td></tr>
                 </tbody>
             </table>
         </div>
@@ -108,17 +51,17 @@
                 <div class="col-md-6">
                     <table class="table">
                         <tr ><th class="text-center" colspan="3">輸出</th></tr>
-                        <tr><th>電壓</th><td><?php echo $t_ac_output_voltage; ?></td><th>V</th></tr>
-                        <tr><th>電流</th><td><?php echo $t_ac_output_current; ?></td><th>A</th></tr>
-                        <tr><th>輸出功率</th><td><?php echo $t_ac_output_wattage; ?></td><th>W</th></tr>
+                        <tr><th>電壓</th><td><?php echo $pvs->dc_output->H5->voltage; ?></td><th>V</th></tr>
+                        <tr><th>電流</th><td><?php echo $pvs->dc_output->H5->current; ?></td><th>A</th></tr>
+                        <tr><th>輸出功率</th><td><?php echo $pvs->dc_output->H5->wattage; ?></td><th>W</th></tr>
                     </table>
                 </div>
                 <div class="col-md-6">
                     <table class="table">
                         <tr><th class="text-center" colspan="3">環境條件</th></tr>
-                        <tr><th>日照強度</th><td>N/A</td><th>W/m2</th></tr>
-                        <tr><th>環境溫度</th><td>N/A</td><th>C</th></tr>
-                        <tr><th>模板溫度</th><td>N/A</td><th>C</th></tr>
+                        <tr><th>UV指數</th><td><?php echo $pvs->environment->uv_index; ?></td><th></th></tr>
+                        <tr><th>環境溫度</th><td><?php echo $pvs->environment->temperature; ?></td><th>C</th></tr>
+                        <tr><th>能見度</th><td><?php echo $pvs->environment->visibility; ?></td><th>C</th></tr>
                     </table>
                 </div>
             </div>
