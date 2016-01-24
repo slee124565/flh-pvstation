47ba039c1880038449bc10d3eb5cc7b60e3cc96c
Lee Shiueh <lee.shiueh@gmail.com>
2016-01-24 12:11:35 +0800

    pvstation template page revise

--- wp-content/themes/totomo/page-templates/page_pvstation.php
+++ wp-content/themes/totomo/page-templates/page_pvstation.php
@@ -14,7 +14,6 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-        <h2> PVStation Template Page</h2>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
