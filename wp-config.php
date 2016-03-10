7f616eec6afd7ccca466b5ef5b6939656629f7b2
Lee Shiueh <lee.shiueh@gmail.com>
2016-03-10 15:53:55 +0800

    revise db config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'pvstation');
+define('DB_NAME', 'raspi');
 
 /** MySQL database username */
-define('DB_USER', 'pvstation');
+define('DB_USER', 'raspi');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'raspberry');
+define('DB_PASSWORD', 'WeKtqjUtGExPnDYt');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
